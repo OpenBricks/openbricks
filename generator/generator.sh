ac0bd1fcb4c4fe00bfb4e1c370470c21df17e07a
Aurelien Jacobs <aurel@gnuage.org>
2003-11-12 17:04:30 +0000

    added romanian translation

--- generator/generator.sh
+++ generator/generator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Menu langage (br/cat/cz/en/es/fi/fr/it/nl/ru/se/sk)
+# Menu langage (br/cat/cz/en/es/fi/fr/it/nl/ro/ru/se/sk)
 # This have no effect on DVD language. See iso/GEEXBOX/etc/mplayer/mplayer.conf
 # For ru, you will need to replace the ttf file in
 # iso/GEEXBOX/usr/share/mplayer/font by a KOI8R ttf font.
