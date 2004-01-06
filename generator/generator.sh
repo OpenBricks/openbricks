512d42ed3815476b42bd2d65fc2c47adbaf71c06
Aurelien Jacobs <aurel@gnuage.org>
2004-01-06 23:00:45 +0000

    put the isolinux.bin on the very beginning of the CD to avoid boot problems on many computers

--- generator/generator.sh
+++ generator/generator.sh
@@ -48,7 +48,7 @@ for i in iso/*; do
   [ "$i" != iso/GEEXBOX ] && ln -s "../$i" ziso
 done
 
-mkisofs -quiet -no-pad -V GEEXBOX -volset GEEXBOX -P "The GeeXboX team (http://www.geexbox.org/)" -p "The GeeXboX team (http://www.geexbox.org/)" -A "MKISOFS ISO 9660/HFS FILESYSTEM BUILDER" -z -f -D -r -J -b GEEXBOX/boot/isolinux.bin -c GEEXBOX/boot/boot.catalog -no-emul-boot -boot-load-size 4 -boot-info-table ziso > geexbox-`cat VERSION`.iso
+mkisofs -quiet -no-pad -V GEEXBOX -volset GEEXBOX -P "The GeeXboX team (http://www.geexbox.org/)" -p "The GeeXboX team (http://www.geexbox.org/)" -A "MKISOFS ISO 9660/HFS FILESYSTEM BUILDER" -z -f -D -r -J -b GEEXBOX/boot/isolinux.bin -c GEEXBOX/boot/boot.catalog -sort sort -no-emul-boot -boot-load-size 4 -boot-info-table ziso > geexbox-`cat VERSION`.iso
 
 rm -rf ziso
 
