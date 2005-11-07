ba0fe89a12121cb6f48d95a207b7e2d7f58fb19a
Amir Shalem <amir@boom.org.il>
2005-11-07 12:25:25 +0000

    avoid failures when deleting /tmp/chans

--- packages/installator/scripts/installator.sh
+++ packages/installator/scripts/installator.sh
@@ -171,7 +171,7 @@ setup_tvscan () {
   if [ -s /tmp/chans ]; then
     echo -n "tv=" | cat - /tmp/chans >> $MPLAYER_CONF
   fi
-  rm /tmp/chans
+  rm -f /tmp/chans
 
   sed -i "s/^TVIN_STANDARD=.*/TVIN_STANDARD=$NORM/" $1/etc/tvcard
   sed -i "s/^CHANLIST=.*/CHANLIST=$CHANLIST/" $1/etc/tvcard
