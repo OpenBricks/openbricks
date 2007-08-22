40dcd50fb6888caa092ddd1eed3dfe3db4ee3938
Andrew Calkin <calkina@geexbox.org>
2007-08-22 23:14:01 +0800

    Clean up webgui code- cosmetic and declaration-based changes

--- packages/webgui/sources/system.c
+++ packages/webgui/sources/system.c
@@ -12,14 +12,14 @@
 */
 
 void printGeexboxControls() {
+	FILE *ptr = fopen("/proc/asound/pcm","r");
+	char data[255];
 	printf("<table width=\"100%%\">");
 	printf("<tr>");
 	printf("<td>");
 	printf("	<a href=\"?system=/usr/bin/tvswitch\"><img src=\"/img/big/tv.png\" title=\"%s\"></a>\n", "Switch TV-Out");
 	printf("</td>");
 	printf("<td align=\"right\">");
-	// test // printf("	<a href=\"?system=/mnt/nfs/video/cpWebgui\"><img src=\"/img/big/update.png\" title=\"%s\"></a>\n", "Update");
-	// test // printf("	&nbsp;");
 	printf("	<a href=\"?system=reboot\"><img src=\"/img/big/reboot.png\" title=\"%s\"></a>\n", REBOOT);
 	printf("	&nbsp;");
 	printf("	<a href=\"?system=poweroff\"><img src=\"/img/big/off.png\" title=\"%s\"></a>\n", POWEROFF);
@@ -28,9 +28,6 @@ void printGeexboxControls() {
 	printf("<br>");
 	
 	printf("<h3>/proc/asound/pcm</h3>");
-
-	FILE *ptr = fopen("/proc/asound/pcm","r");
-	char data[255];
 	while (feof(ptr) == 0) {
 		fgets(data, 255, ptr);
 		printf("%s<br>", data);
@@ -40,15 +37,14 @@ void printGeexboxControls() {
 }
 
 void printPlayerState() {
-	char data[MAX];
+	char data[MAX], *x;
 	FILE *ptr;
 
 	ptr = fopen("/tmp/mp_streaminfo","r");
 	fgets(data, MAX, ptr);
 	
-	char* x = strchr(data, '=') + (1 * sizeof(char));
+	x = strchr(data, '=')+1;
 
 	printf("%s", x);
-
 	fclose(ptr);	
 }
