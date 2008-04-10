7137da18ee8dc71217ec19d235ca8b9e009636cc
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2008-04-10 17:38:39 +0200

    cosmetic: fix indentation

--- packages/webgui/sources/index.c
+++ packages/webgui/sources/index.c
@@ -169,7 +169,7 @@ void printPageHead() {
 	printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 	printf("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n");
 	printf("<html xmlns=\"http://www.w3.org/1999/xhtml\"\n");
-     printf("      xml:lang=\"en\" >\n");
+	printf("      xml:lang=\"en\" >\n");
 	printf("\t<head>\n");
 	printf("\t\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n");
 	printf("\t\t<title>GeeXboX &#8250; WebGui %s</title>\n", VERSION);
