ae5e3ef491cb9e2d70a804014f4368ed668160b1
Thomas Genty <tomlohave@openbricks.org>
2011-12-10 10:49:16 +0100

    xbmc : prepare to eden ...

--- packages/xbmc/scripts/xbmc-default-settings.sh
+++ packages/xbmc/scripts/xbmc-default-settings.sh
@@ -6,44 +6,17 @@
 #############################################################################
 
 USERDATA="$HOME/.xbmc/userdata"
-ADDONS="$HOME/.xbmc/addons"
-TEMP_DIR="$HOME/.xbmc/temp"
 ADV_SETTINGS="$USERDATA/advancedsettings.xml"
-GUI_SETTINGS="$USERDATA/guisettings.xml"
 SOURCES="$USERDATA/sources.xml"
-
-gpu_guess () {
-
-  GPUDEVICE=$(cat /tmp/pci | grep 0300)
-  GPUTYPE="OTHER"
-  [ "$(echo $GPUDEVICE | grep 8086)" ] && GPUTYPE="INTEL"
-  [ "$(echo $GPUDEVICE | grep 10de)" ] && GPUTYPE="NVIDIA"
-  [ "$(echo $GPUDEVICE | grep 1002)" ] && GPUTYPE="AMD"
-
-  export GPUTYPE
-}
-
-set_default_gui_settings () {
-  [ -f "$GUI_SETTINGS" ] && return
-
-  [ "$GPUTYPE" != "NVIDIA" -a "$GPUTYPE" != "AMD" ] && return
-
-  cat > "$GUI_SETTINGS" << EOF
-<settings>
-  <!-- Sync to vblank -->
-  <videoscreen>
-    <vsync>2</vsync>
-  </videoscreen>
-</settings>
-EOF
-}
 
 set_default_advanced_settings () {
   [ -f "$ADV_SETTINGS" ] && return
 
+
   cat > "$ADV_SETTINGS" << EOF
 <advancedsettings>
   <useddsfanart>true</useddsfanart>
+  <cputempcommand>sed -e 's/\([0-9]*\)[0-9]\{3\}.*/\1 C/' /sys/class/thermal/thermal_zone0/temp</cputempcommand>
   <samba>
     <clienttimeout>10</clienttimeout>
   </samba>
@@ -84,22 +57,9 @@ set_default_sources () {
 EOF
 }
 
-# clean temp dir
-rm -rf "$TEMP_DIR/*"
-
 mkdir -p "$USERDATA"
-gpu_guess
 set_default_advanced_settings
-set_default_gui_settings
 set_default_sources
 
 # remote
 [ -f /usr/share/xbmc/system/Lircmap.xml ] && cp /usr/share/xbmc/system/Lircmap.xml "$USERDATA"
-
-# geexbox.network.cfg
-mkdir -p $ADDONS
-[ -d /usr/share/xbmc/addons/geexbox.network.cfg ]  && cp -R /usr/share/xbmc/addons/geexbox.network.cfg "$ADDONS"
-
-# geexbox.updater
-mkdir -p $ADDONS
-[ -d /usr/share/xbmc/addons/geexbox.updater ]  && cp -R /usr/share/xbmc/addons/geexbox.updater "$ADDONS"
