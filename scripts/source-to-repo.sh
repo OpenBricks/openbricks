01aff1f7cc2c5021363a1c475cc552d19adbd2d2
Thomas Genty <tomlohave@openbricks.org>
2012-03-04 07:21:24 +0100

    scripts : add new script : source-to-repo (Peter Meerwald)

---
+++ scripts/source-to-repo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "checkurl_dl.sh: Using checkurl output: $1"
+echo "Downloading files missing on sources.openbricks.org..."
+
+while read line
+do
+
+  if [ ${line##* } != "failed" ]
+  then
+
+    url=`echo $line | cut -d \   -f 5`
+
+    echo -n "Downloading $url... "
+    wget --no-check-certificate --passive-ftp --dns-timeout=5 --connect-timeout=5 --read-timeout=5 --tries=1 -c -q $url
+    if [ $? = 0 ]
+    then
+      echo OK
+    else
+      echo FAILED
+    fi
+
+  fi
+
+done < $1
+
