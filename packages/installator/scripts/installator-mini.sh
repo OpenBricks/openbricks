11c7f9fef81259abc4d289ca14b3d84e722a9b1f
Johannes Schwanzer <johannes@schwan-net.com>
2012-04-23 21:27:05 +0200

    installator and grub-efi: Added EFI based installation to the installator script, Added the use of GNU Parted for optimal disk alignment (SSD) and simplicity of partition creation to the installator script, Added some modules to EFI bootloader created with the grub-efi package

--- packages/installator/scripts/installator-mini.sh
+++ packages/installator/scripts/installator-mini.sh
@@ -37,7 +37,7 @@ rdev=`cat /proc/mounts | grep '/\.root' | cut -f1 -d\ | cut -f3 -d/`
 for i in /sys/block/*; do
   dev=`basename $i`
 
-  [ "$dev" =  loop ] && continue
+  [ "$dev" = loop ] && continue
   [ "$dev" = ram ] && continue
   [ "$dev" = "$rdev" ] && continue
   [ -r /sys/block/$dev/device/type ] || continue
@@ -73,6 +73,11 @@ fi
 until [ -n "$DISK" ] && echo "$disklist" | grep -q "$DISK"; do
   read -p "Where to you want to install $DISTRO ($disklist) ? " DISK
 done
+echo
+
+/usr/sbin/efibootmgr > /dev/null 2>&1
+[ $? = 0 ] && yesnoquestion "Do you want an EFI based installation?" && efi_install=y
+echo
 
 echo "----------- WARNING --------------"
 echo "$DISTRO will be installed on $DISK"
@@ -80,38 +85,108 @@ echo "All the contents of $DISK will be erased!"
 
 yesnoquestion "Are you sure you want to continue" || abort_install
 
+echo
 echo "Let me say that again: ALL the contents of $DISK will be ERASED"
 yesnoquestion "Are you REALLY sure you want to continue" || abort_install
 
+echo
 echo "Repartitioning..." 
 target_dev="/dev/$DISK"
-dd if=/dev/zero of=$target_dev bs=512 count=4
-/bin/echo -e 'o\nn\np\n1\n1\n\na\n1\nw\n' | fdisk $target_dev
+target_fs=ext4
+# Make sure the ESP size is big enough to get FAT32!
+esp_size=200MB
+part_table=msdos
+[ "$efi_install" = y ] && part_table=gpt
+
+# Erease disk and create table
+/usr/sbin/parted -s $target_dev mktable $part_table
 target_part=${target_dev}1
+
+if [ "$efi_install" = y ]; then
+  # Create ESP (EFI System Partition) and root partition if an EFI based installation requested
+  efi_part=${target_dev}1
+  /usr/sbin/parted -s --align=opt $target_dev mkpart primary 0% $esp_size
+  /sbin/mkfs.vfat $efi_part
+
+  target_part=${target_dev}2
+  /usr/sbin/parted -s --align=opt $target_dev mkpart primary $esp_size 100%
+else
+  # Otherwise create a full partition on whole disk
+  /usr/sbin/parted -s --align=opt $target_dev mkpart primary 0% 100%
+fi
+# Setting boot flag is important especially for the ESP with GPT table
+/usr/sbin/parted -s $target_dev set 1 boot on
+
+echo
+echo "Current layout of disk $DISK:"
+/usr/sbin/parted -s $target_dev print
+
+
 echo "Formatting..." 
-/usr/sbin/mkfs.ext4 -L $DISTRO -m 0 $target_part
-/usr/sbin/tune2fs -c 0 -i 0 $target_part
+/usr/sbin/mkfs.$target_fs -L $DISTRO -m 0 $target_part > /dev/null 2>&1 
+/usr/sbin/tune2fs -c 0 -i 0 $target_part > /dev/null 2>&1 
 mkdir -p /tmp/installator
 /sbin/blkid -o udev $target_part > /tmp/installator/blkid
 . /tmp/installator/blkid
+
 echo "Installing $DISTRO..."
 target="/tmp/installator/target"
 mkdir -p $target
-mount $target_part $target
+mount -t $target_fs $target_part $target
 cp -PR /.squashfs/* $target
+
 cat > $target/etc/fstab <<EOF
 proc /proc proc defaults 0 0
-UUID=${ID_FS_UUID} / ext4 relatime,errors=remount-ro 0 1
+UUID=${ID_FS_UUID} / $target_fs relatime,errors=remount-ro 0 1
 EOF
+
 echo "Installing the kernel..."
 mkdir -p $target/boot
 cp -P /.root/vmlinuz $target/boot/
 cp -P /.root/initrd $target/boot/
-echo "Installing the boot loader..."
-cp -P /.root/isolinux/splash.png $target/boot/
-cp -P /.root/isolinux/vesamenu.c32 $target/boot/
-cp -P /.root/isolinux/help.msg $target/boot/
-cat > $target/boot/syslinux.cfg <<EOF
+
+# Use the right bootloader if EFI is requested
+if [ "$efi_install" = y ]; then
+  echo "Installing the boot loader..."
+  efi="/tmp/installator/efi"
+  mkdir -p $efi
+  mount -t vfat $efi_part $efi
+  cp -PR /.root/efi $efi
+  cat > $efi/efi/boot/grub.cfg <<EOF
+set prefix=/efi/boot
+set default=0
+set timeout=10
+
+insmod efi_gop
+insmod efi_uga
+insmod font
+
+if loadfont \$prefix/8x13.pf2
+then
+    insmod gfxterm
+    set gfxmode=auto
+    set gfxpayload=keep
+    terminal_output gfxterm
+fi
+
+menuentry "Start $DISTRO $ARCH $VERSION" {
+  search --no-floppy --fs-uuid --set=root ${ID_FS_UUID}
+  linux /boot/vmlinuz root=UUID=${ID_FS_UUID} rootfstype=${target_fs} quiet loglevel=3 rw
+  initrd /boot/initrd
+}
+menuentry "Start in debuging mode" {
+  search --no-floppy --fs-uuid --set=root ${ID_FS_UUID}
+  linux /boot/vmlinuz root=UUID=${ID_FS_UUID} rootfstype=${target_fs} emergency
+  initrd /boot/initrd
+}
+EOF
+# Otherwise use a MBR/Bios based installation with Isolinux
+else
+  echo "Installing the boot loader..."
+  cp -P /.root/isolinux/splash.png $target/boot/
+  cp -P /.root/isolinux/vesamenu.c32 $target/boot/
+  cp -P /.root/isolinux/help.msg $target/boot/
+  cat > $target/boot/syslinux.cfg <<EOF
 DEFAULT /boot/vesamenu.c32
 PROMPT 0
 
@@ -144,9 +219,10 @@ LABEL debug
 F1 help.msg #00000000
 EOF
 
-/sbin/extlinux --install $target/boot
-umount $target
-cat /usr/share/syslinux/mbr.bin > $target_dev
+  /sbin/extlinux --install $target/boot
+  umount $target
+  cat /usr/share/syslinux/mbr.bin > $target_dev
+fi
 
 echo "The installation has been completed!"
 read -p "Press any key to reboot the system..." DUMMY
