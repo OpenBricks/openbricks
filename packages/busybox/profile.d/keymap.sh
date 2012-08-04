eb907d01a839743bae007de9e3b6bb5d6f932048
Johannes Schwanzer <johannes@schwan-net.com>
2012-08-04 23:01:29 +0200

    Update and harmonize the use of CONFIG_OPT_KEYMAP

--- packages/busybox/profile.d/keymap.sh
+++ packages/busybox/profile.d/keymap.sh
@@ -2,5 +2,5 @@
 
 # load keymap given on kernel cmdline
 keymap=$(sed -n "s/.*keymap=\([^ ]*\).*/\1/p" /proc/cmdline)
-[ "x$keymap" != "x" ] && [ -r /etc/keymaps/$keymap ] && cat /etc/keymaps/$keymap | /sbin/loadkmap
+[ "x$keymap" != "x" ] && [ -r /etc/keymaps/${keymap}.kmap ] && cat /etc/keymaps/${keymap}.kmap | /sbin/loadkmap
 
