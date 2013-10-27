acbde57486146cc7f4bfa87ab322d79c8f51f786
warped-rudi <r.ihle@s-t.de>
2013-10-27 21:01:33 +0100

    xbmc : cleanup and fix early setip script

--- packages/xbmc/scripts/xbmc-default-settings.sh
+++ packages/xbmc/scripts/xbmc-default-settings.sh
@@ -10,75 +10,70 @@ ADV_SETTINGS="$USERDATA/advancedsettings.xml"
 GUI_SETTINGS="$USERDATA/guisettings.xml"
 CORE_FACTORY="$USERDATA/playercorefactory.xml"
 SOURCES="$USERDATA/sources.xml"
+KEY_MAP="$USERDATA/Lircmap.xml"
+SYS_PREFIX=""
 
-set_default_advanced_settings () {
-  [ -f "$ADV_SETTINGS" ] && return
+set_system_prefix () {
+  [ ! -z $SYS_PREFIX ] && return
 
-  if grep -q "OMAP4 Panda board" /proc/cpuinfo; then
-    cp /etc/xbmc/panda-advancedsettings.xml $ADV_SETTINGS
-    cp /etc/xbmc/panda-playercorefactory.xml $CORE_FACTORY
-  elif grep -q Snowball /proc/cpuinfo; then
-    cp /etc/xbmc/snowball-advancedsettings.xml $ADV_SETTINGS
-  elif grep -q CuBox /proc/cpuinfo; then
-    cp /etc/xbmc/cubox-advancedsettings.xml $ADV_SETTINGS
-  elif grep -q BCM2708 /proc/cpuinfo; then
-    cp /etc/xbmc/rpi-advancedsettings.xml $ADV_SETTINGS
-  elif grep -q sun4i /proc/cpuinfo; then
-    cp /etc/xbmc/a10-advancedsettings.xml $ADV_SETTINGS
-  elif grep -q CM-FX6 /proc/cpuinfo; then
-    cp /etc/xbmc/utilite-advancedsettings.xml $ADV_SETTINGS
-  elif grep -q "SolidRun i.MX 6" /proc/cpuinfo; then
-    cp /etc/xbmc/cuboxi-advancedsettings.xml $ADV_SETTINGS
-  else
-    cp /etc/xbmc/generic-advancedsettings.xml $ADV_SETTINGS
+  if   grep -q "OMAP4 Panda board" /proc/cpuinfo; then SYS_PREFIX="panda"
+  elif grep -q "Snowball" /proc/cpuinfo;          then SYS_PREFIX="snowball"
+  elif grep -q "CuBox" /proc/cpuinfo;             then SYS_PREFIX="cubox"
+  elif grep -q "BCM2708" /proc/cpuinfo;           then SYS_PREFIX="rpi"
+  elif grep -q "sun4i" /proc/cpuinfo;             then SYS_PREFIX="a10"
+  elif grep -q "CM-FX6" /proc/cpuinfo;            then SYS_PREFIX="utilite"
+  elif grep -q "SolidRun i.MX" /proc/cpuinfo;     then SYS_PREFIX="cuboxi"
+  else                                            SYS_PREFIX="generic"     
   fi
 }
 
-set_default_gui_settings () {
-  [ -f "$GUI_SETTINGS" ] && return
+copy_if_present () {
+  [ -f $1 ] && cp $1 $2
+}
+
+copy_if_missing () {
+  [ ! -f $2 ] && cp $1 $2
+}
+
+
+# create profile directory
+mkdir -p "$USERDATA"
+
+# set default advancedsettings.xml
+if [ ! -f "$ADV_SETTINGS" ] ; then
+  set_system_prefix
+  copy_if_present /etc/xbmc/$SYS_PREFIX-playercorefactory.xml $CORE_FACTORY
+  copy_if_present /etc/xbmc/$SYS_PREFIX-advancedsettings.xml $ADV_SETTINGS
+  copy_if_missing /etc/xbmc/generic-advancedsettings.xml $ADV_SETTINGS
+fi
+
+# set default guisettings.xml
+if [ ! -f "$GUI_SETTINGS" ] ; then
+  set_system_prefix
+  copy_if_present /etc/xbmc/$SYS_PREFIX-guisettings.xml $GUI_SETTINGS
+  copy_if_missing /etc/xbmc/generic-guisettings.xml $GUI_SETTINGS
 
   ZONEINFO="/usr/share/zoneinfo"
-  if [ -z "$TZ" ]; then
+  if [ -z "$TZ" ] && [ -h /etc/localtime ] ; then
     TZ=`readlink /etc/localtime`
     TZ=${TZ#${ZONEINFO}}
-    TZ_COUNTRY_CODE=`grep $TZ $ZONEINFO/zone.tab | cut -f1 | head -1`
   fi
-  [ -n "$TZ_COUNTRY_CODE" ] \
+  [ -n "$TZ" ] && \
+    TZ_COUNTRY_CODE=`grep $TZ $ZONEINFO/zone.tab | cut -f1 | head -1`
+  [ -n "$TZ_COUNTRY_CODE" ] && \
     TZ_COUNTRY=`grep $TZ_COUNTRY_CODE $ZONEINFO/iso3166.tab | cut -f2 | head -1`
 
-  cp /etc/xbmc/guisettings.xml $GUI_SETTINGS
+  sed -i -e "s,TZ_COUNTRY,$TZ_COUNTRY," -e "s,TZ,$TZ," $GUI_SETTINGS
+fi
 
-  if grep -q CuBox /proc/cpuinfo; then
-    cp /etc/xbmc/cubox-guisettings.xml $GUI_SETTINGS
-  fi
-  
-  if grep -q "SolidRun i.MX"  /proc/cpuinfo; then
-    cp /etc/xbmc/cuboxi-guisettings.xml $GUI_SETTINGS
-  fi
+# set default sources.xml
+if [ ! -f "$SOURCES" ] ; then
+  copy_if_present /etc/xbmc/sources.xml $SOURCES
+fi
 
-   if grep -q  CM-FX6 /proc/cpuinfo; then
-     cp /etc/xbmc/utilite-guisettings.xml $GUI_SETTINGS
-   fi
- 
-  sed -i -e "s,TZ_COUNTRY,$TZ_COUNTRY," \
-         -e "s,TZ,$TZ," \
-         $GUI_SETTINGS
-}
-
-set_default_sources () {
-  [ -f "$SOURCES" ] && return
-  cp /etc/xbmc/sources.xml $SOURCES
-}
-
-mkdir -p "$USERDATA"
-set_default_advanced_settings
-set_default_gui_settings
-set_default_sources
-
-# remote
-if [ -f /usr/share/xbmc/system/Lircmap.xml ] && [ ! -f $USERDATA/Lircmap.xml ]
-then
-  cp /usr/share/xbmc/system/Lircmap.xml "$USERDATA"
+# set default remote control key map
+if [ ! -f "$KEY_MAP" ] ; then
+  copy_if_present /usr/share/xbmc/system/Lircmap.xml $KEY_MAP
 fi
 
 exit 0
