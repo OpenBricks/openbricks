4bb39e5df50696adc250b70d2ecd3678ead8d748
Rudi <r.ihle@s-t.de>
2014-11-13 22:03:54 +0100

    busybox: Use bash.bashrc in profile.d/system.sh

--- packages/system/busybox/profile.d/system.sh
+++ packages/system/busybox/profile.d/system.sh
@@ -1,9 +1,8 @@
 # System Settings
 
-# System Settings
-
 export PATH="/bin:/sbin:/usr/bin:/usr/sbin"
 export HOSTNAME=$(cat /etc/hostname)
+
 [ -r /etc/locale.conf ] && . /etc/locale.conf && export LANG
 
 update_ps1() {
@@ -26,17 +25,7 @@ my_cd() {
 
 if ps ax -o pid,comm | grep `echo $$` | grep -q bash ; then
   ## it's bash
-  col_bold="\033[1m"
-  col_red="\033[1;31m"
-  col_green="\033[1;32m"
-  col_yellow="\033[1;33m"
-  col_blue="\033[1;34m"
-  col_purple="\033[1;35m"
-  col_cyan="\033[1;36m"
-  col_normal="\033[0;39m"
-
-  PS1="\[${col_cyan}\]\u@\h:\[${col_blue}\]\w\[${col_normal}\]\$ "
-  export PS1
+  [ -r /etc/bash.bashrc ] && . /etc/bash.bashrc
 else
   ## it's sh from busybox
   alias cd=my_cd
