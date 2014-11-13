c810ee389ef21ad9583afb63b19ac3281f5ea9b0
Rudi <r.ihle@s-t.de>
2014-11-13 22:07:59 +0100

    busybox: Derive keyboard map from language setting in locale.conf

--- packages/system/busybox/profile.d/keymap.sh
+++ packages/system/busybox/profile.d/keymap.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-# load keymap given on kernel cmdline
-keymap=$(sed -n "s/.*keymap=\([^ ]*\).*/\1/p" /proc/cmdline)
-[ "x$keymap" != "x" ] && [ -r /etc/keymaps/${keymap}.kmap ] && cat /etc/keymaps/${keymap}.kmap | /sbin/loadkmap
+loaded="no"
 
+try_keymap() {
+  if [ -n "$1" ] && [ -r /etc/keymaps/$1.kmap ]; then
+    cat /etc/keymaps/$1.kmap | /sbin/loadkmap
+    loaded="yes"
+  fi
+}
+
+# load keymap given in /etc/locale.conf
+if [ -r /etc/locale.conf ]; then
+  try_keymap $(sed -n "s/.*LANG=\([^\.]*\).*/\1/p" /etc/locale.conf)
+
+  [ $loaded = "no" ] && \
+    try_keymap $(sed -n "s/.*LANG=\([^_]*\).*/\1/p" /etc/locale.conf)
+fi
+
+# load keymap given on kernel cmdline
+[ $loaded = "no" ] && \
+  try_keymap  $(sed -n "s/.*keymap=\([^ ]*\).*/\1/p" /proc/cmdline)
