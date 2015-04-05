a7edb3649d78531140e51bb0f4f14c38374b05d5
Rudi <r.ihle@s-t.de>
2015-04-05 18:01:25 +0200

    networkmanager: Fix dispatcher script invocation, add dependency to dbus

--- packages/net/networkmanager/scripts/ntpd.sh
+++ packages/net/networkmanager/scripts/ntpd.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-. /etc/network
+NM_IFACE="$1"
+NM_STATE="$2"
 
-for i in $TIMESERVERS; do
-  NTP_OPTS="$NTP_OPTS -p $i"
-done
+if [ "$NM_STATE" = "up" ]; then
+  . /etc/network
 
-echo "Starting ntpd"
-/usr/sbin/ntpd $NTP_OPTS
+  if [ -n "$TIMESERVERS" ] && [ "$IFACE" = "$NM_IFACE" ]; then
+    for i in $TIMESERVERS; do
+      NTP_OPTS="$NTP_OPTS -p $i"
+    done
+
+    echo "Starting ntpd"
+    /usr/sbin/ntpd $NTP_OPTS
+  fi
+fi
+
+exit 0
