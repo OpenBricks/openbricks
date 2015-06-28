d011708b67f5b53f1e4ef26ccd724079af9cd2a5
Rudi <r.ihle@s-t.de>
2015-06-28 20:01:06 +0200

    busybox: Optimize keymap.sh script

--- packages/system/busybox/profile.d/keymap.sh
+++ packages/system/busybox/profile.d/keymap.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-loaded="no"
+loaded=""
 
 try_keymap() {
-  if [ -n "$1" ] && [ -r /etc/keymaps/$1.kmap ]; then
-    cat /etc/keymaps/$1.kmap | /sbin/loadkmap
-    loaded="yes"
+  if [ -z "$loaded" ] && [ -n "$1" ] && [ -r /etc/keymaps/$1.kmap ]; then
+    /sbin/loadkmap < /etc/keymaps/$1.kmap
+    loaded=1
   fi
 }
 
 # load keymap given in /etc/locale.conf
 if [ -r /etc/locale.conf ]; then
   try_keymap $(sed -n "s/.*LANG=\([^\.]*\).*/\1/p" /etc/locale.conf)
-
-  [ $loaded = "no" ] && \
-    try_keymap $(sed -n "s/.*LANG=\([^_]*\).*/\1/p" /etc/locale.conf)
+  try_keymap $(sed -n "s/.*LANG=\([^_]*\).*/\1/p" /etc/locale.conf)
 fi
 
 # load keymap given on kernel cmdline
-[ $loaded = "no" ] && \
-  try_keymap  $(sed -n "s/.*keymap=\([^ ]*\).*/\1/p" /proc/cmdline)
+try_keymap $(sed -n "s/.*keymap=\([^ ]*\).*/\1/p" /proc/cmdline)
