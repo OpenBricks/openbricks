4944d4ec9e61ad13d14ed2477df2aae03a275801
Thomas Genty <tomlohave@geexbox.org>
2017-06-10 16:02:04 +0200

    doozer : show our public key

--- scripts/Autobuild.sh
+++ scripts/Autobuild.sh
@@ -3,45 +3,37 @@
 # $1 is target
 
 # In case it's the first time we try the build
-mkdir -p /project/sources /project/stamps /project/build.host/.cache build/build.host build/config /project/.ccache
+mkdir -p /project/sources /project/stamps /project/build.host/.cache build/build.host build/config /project/.ccache /project/.ssh
 
-# Do not dowmnload if we have already the sources
-ln -s /project/sources sources
-ln -s /project/stamps .stamps
+create_img () {
+  local name=`basename *.tar.xz .tar.xz`
+  if [ -f make-sdcard ]; then
+    sudo rm -f /tmp/$CONFNAME.img*
+    sudo ./make-sdcard /tmp/$CONFNAME.img $name.tar.xz
+    sudo chown --reference=$name.tar.xz /tmp/$CONFNAME.img*
+    [ -f /tmp/$CONFNAME.img.xz ] && mv /tmp/$CONFNAME.img.xz ./$name.img.xz
+    rm -f /tmp/$CONFNAME.img*
+  fi
+}
+REPONAME=openbricks
+REPO=/project/repo/checkout
+CONFNAME=$1
+CONFFILE=$REPO/config/defconfigs/$1.conf
+DATE=$(date +%Y%m%d)
 
-# Nothing for ccache, already embedded in our scripts
-
-# Restore build.host, it saves 6-7 min 
-echo "Restoring build.host..."
-cp -r /project/build.host/ build/
-
-# Just to see...
-echo "Content of build/build.host"
-ls -al build/build.host
-echo "Content of build/build.host/.cache"
-ls -al build/build.host/.cache
-echo "Content of /project/sources"
-ls -al /project/sources
-echo "Content of /project"
-ls -al /project
-
-
-echo "######################"
-echo "# Starting the build #"
-echo "######################"
-./scripts/loadcfg $1 || exit 1
-# Build with the maximum speed (maybe we should try too without MAKECFLAGS set)
-DOOZER_CONCURRENCY_MAKE_LEVEL=$(echo $MAKEFLAGS |cut -d, -f2)
-echo "Using DOOZER_CONCURRENCY_MAKE_LEVEL=$DOOZER_CONCURRENCY_MAKE_LEVEL : MAKEFLAGS was $MAKEFLAGS"
-echo DOOZER_CONCURRENCY_MAKE_LEVEL=$DOOZER_CONCURRENCY_MAKE_LEVEL >> build/config/options-doozer
-make || exit 1
-
-# keep build.host
-if [ -n "$(ls -A /project/build.host/.cache)" ] ; then 
-  echo "Nothing to save in /project/build.host"
+# Create a ssh key for our server
+if [ -f /project/.ssh/id_rsa ] ; then
+  echo "[ssh] Not generating a new file, already here"
 else
- echo "Copying build/build.host to /project"
- cp -r build/build.host /project/
+  ssh-keygen -q -t rsa -N "" -f /project/.ssh/id_rsa
 fi
+ssh_opt="-o UserKnownHostsFile=/project/.ssh/known_hosts -o StrictHostKeyChecking=no"
+echo "######## Public key #########"
+cat /project/.ssh/id_rsa.pub
+echo "######## Public key #########"
 
-# Should we publish our opkg files ?
+
+
+
+
+
