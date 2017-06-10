21e982e35e99cd26059066e5b78a8760a3685364
Thomas Genty <tomlohave@geexbox.org>
2017-06-10 16:08:39 +0200

    doozer : add more targets, uploads our files to fry (try1)

--- scripts/Autobuild.sh
+++ scripts/Autobuild.sh
@@ -32,7 +32,74 @@ echo "######## Public key #########"
 cat /project/.ssh/id_rsa.pub
 echo "######## Public key #########"
 
+# Do not dowmnload if we have already the sources
+ln -s /project/sources sources
+ln -s /project/stamps .stamps
 
+# Nothing for ccache, already embedded in our scripts
+
+# Restore build.host, it saves 6-7 min 
+echo "Restoring build.host..."
+cp -r /project/build.host/ build/
+
+# Just to see...
+# echo "Content of build/build.host"
+# ls -al build/build.host
+# echo "Content of build/build.host/.cache"
+# ls -al build/build.host/.cache
+# echo "Content of /project/sources"
+# ls -al /project/sources
+# echo "Content of /project"
+# ls -al /project
+
+
+echo "######################"
+echo "# Starting the build #"
+echo "######################"
+./scripts/loadcfg $1 || exit 1
+# Build with the maximum speed (maybe we should try too without MAKECFLAGS set)
+DOOZER_CONCURRENCY_MAKE_LEVEL=$(echo $MAKEFLAGS |cut -d, -f2)
+echo "Using DOOZER_CONCURRENCY_MAKE_LEVEL=$DOOZER_CONCURRENCY_MAKE_LEVEL : MAKEFLAGS was $MAKEFLAGS"
+echo DOOZER_CONCURRENCY_MAKE_LEVEL=$DOOZER_CONCURRENCY_MAKE_LEVEL >> build/config/options-doozer
+
+if grep -q 'CONFIG_OPT_TARGET_FLAT=y' $CONFFILE; then
+  sed \
+      -e 's:CONFIG_OPT_TARGET_FLAT=y:# CONFIG_OPT_TARGET_FLAT is not set:' \
+      -e 's:# CONFIG_OPT_TARGET_TARBALL is not set:CONFIG_OPT_TARGET_TARBALL=y:' \
+       < $CONFFILE > `ls -d build/build.host/kconfig-frontends-*`/.config
+else
+  cp -P $CONFFILE `ls -d build/build.host/kconfig-frontends-*`/.config
+fi
+
+make silentoldconfig
+
+
+make || exit 1
+
+# keep build.host
+if [ -n "$(ls -A /project/build.host/.cache)" ] ; then 
+  echo "Nothing to save in /project/build.host"
+else
+ echo "Copying build/build.host to /project"
+ cp -r build/build.host /project/
+fi
+
+########################### Sending our files now #########################
+# Clean packages
+echo "Cleaning binaries/binaries.*"
+find binaries/binaries.* -name "*-dbg_*.opk" -delete
+
+mkdir -p /project/$REPONAME/$CONFNAME/$DATE
+rm -rf /project/$REPONAME/$CONFNAME/$DATE/*
+
+echo "Moving files to /project/$REPONAME/$CONFNAME/$DATE"
+mv binaries/binaries.* /project/$REPONAME/$CONFNAME/$DATE
+
+(cd /project/$REPONAME/$CONFNAME/$DATE/binaries.*; create_img)
+
+scp $ssh_opt -i /project/.ssh/id_rsa -v -r /project/$REPONAME buildbot@geexbox.org:/data/snapshots/data
+
+rm -rf /project/$REPONAME/$CONFNAME/$DATE
 
 
 
